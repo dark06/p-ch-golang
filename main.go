8f6ad4d759580fdc938082c8a557c73a0aa1e685
Abram Lawendy <28400414+dark06@users.noreply.github.com>
2025-03-22 18:20:38 +0100

    fix(main): body not extracted correctly
    Signed-off-by: Abram Lawendy <28400414+dark06@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io"
 	"net/http"
@@ -8,22 +9,30 @@ import (
 	"time"
 )
 
+type HTMLParser struct {
+	xml.Name
+	BodyText string `xml:"body"`
+}
+
 func main() {
 	client := &http.Client{Timeout: 10 * time.Second}
 
-	respone, err := client.Get("http://172.31.28.67")
+	response, err := client.Get("http://172.31.28.67")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	body, err := io.ReadAll(respone.Body)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	if respone.StatusCode == 200 && string(body) == "Hello World" {
+	extractedText := &HTMLParser{}
+	xml.Unmarshal([]byte(body), extractedText)
+	if response.StatusCode == 200 && extractedText.BodyText == "Hello World" {
 		fmt.Println("Ok")
 		os.Exit(0)
 	} else {
